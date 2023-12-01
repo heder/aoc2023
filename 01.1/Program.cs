52b6ae2eb50959ed3db8332d720eb8de84d80d38
Thomas Heder <thomas.heder@gmail.com>
2023-12-01 07:52:20 +0100

    1

--- 01.1/Program.cs
+++ 01.1/Program.cs
@@ -4,23 +4,19 @@
     {
         var lines = File.ReadLines("in.txt").ToArray();
 
-        int highest = 0;
-        int curr = 0;
+        int sum = 0;
         for (int i = 0; i < lines.Length; i++)
         {
-            if (lines[i] == "")
-            {
-                highest = Math.Max(curr, highest);
-                curr = 0;
-                continue;
-            }
+            var f = lines[i].First(f => char.IsNumber(f));
+            var l = lines[i].Last(f => char.IsNumber(f));
 
-            curr += Convert.ToInt32(lines[i]);
+            string value = $"{f}{l}";
+
+            sum += Convert.ToInt32(value);
+
         }
 
-        highest = Math.Max(curr, highest);
-
-        Console.WriteLine(highest);
+        Console.WriteLine(sum);
         Console.ReadKey();
     }
 }
--- 01.2/Program.cs
+++ 01.2/Program.cs
@@ -1,28 +1,105 @@
-﻿class Program
+﻿using System.Runtime.ExceptionServices;
+
+class Program
 {
     static void Main()
     {
-        var lines = File.ReadLines("in.txt").ToArray();
-        var groups = new List<int>();
+        var number = new Dictionary<string, int>()
+        {
+            { "one", 1 },
+            { "two", 2},
+            { "three", 3},
+            { "four", 4},
+            { "five", 5},
+            { "six", 6},
+            { "seven", 7},
+            { "eight", 8},
+            { "nine", 9},
+        };
 
-        int curr = 0;
+
+        var lines = File.ReadLines("in.txt").ToArray();
+
+        int sum = 0;
         for (int i = 0; i < lines.Length; i++)
         {
-            if (lines[i] == "")
+            string s = lines[i];
+
+            var firstNum = s.FirstOrDefault(f => char.IsNumber(f));
+            var firstNumAt = s.IndexOf(firstNum);
+            if (firstNumAt == -1) firstNumAt = int.MaxValue;
+
+            var firstTextVal = -1;
+            var firstTextAt = int.MaxValue;
+
+            foreach (var item in number)
             {
-                groups.Add(curr);
-                curr = 0;
-                continue;
+                int x = s.IndexOf(item.Key);
+                if (x > -1)
+                {
+                    if (x < firstTextAt)
+                    {
+                        firstTextAt = x;
+                        firstTextVal = item.Value;         
+                    }
+                }
             }
 
-            curr += Convert.ToInt32(lines[i]);
+            int fn;
+            if (firstNumAt < firstTextAt)
+            {
+                fn = Convert.ToInt32(firstNum.ToString());
+            }
+            else
+            {
+                fn = firstTextVal;
+            }
+
+
+
+
+            var sRev = new string(s.ToCharArray().Reverse().ToArray());
+
+            var lastNum = sRev.FirstOrDefault(f => char.IsNumber(f));
+            var lastNumAt = sRev.IndexOf(lastNum);
+            if (lastNumAt == -1) lastNumAt = int.MaxValue;
+
+            var lastTextVal = -1;
+            var lastTextAt = int.MaxValue;
+
+            foreach (var item in number)
+            {
+                string revKey = new string(item.Key.ToCharArray().Reverse().ToArray());
+                int x = sRev.IndexOf(revKey);
+                if (x > -1)
+                {
+                    if (x < lastTextAt)
+                    {
+                        lastTextAt = x;
+                        lastTextVal = item.Value;
+                    }
+                }
+            }
+
+            int ln;
+            if (lastNumAt < lastTextAt)
+            {
+                ln = Convert.ToInt32(lastNum.ToString());
+            }
+            else
+            {
+                ln = lastTextVal;
+            }
+
+
+
+            string value = $"{fn}{ln}";
+
+            sum += Convert.ToInt32(value);
+
         }
 
-        groups.Add(curr);
-
-        var x = groups.OrderByDescending(f => f).Take(3).Sum();
-
-        Console.WriteLine(x);
+        Console.WriteLine(sum);
         Console.ReadKey();
     }
 }
