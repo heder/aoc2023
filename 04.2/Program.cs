216c8cde1d034de3d16a6956576c2440e0d34b52
Thomas Heder <thomas.heder@sogeti.com>
2023-12-04 20:42:59 +0100

    4

--- 04.2/Program.cs
+++ 04.2/Program.cs
@@ -1,40 +1,60 @@
 ﻿class Program
 {
-    static void Main()
+    class Card
     {
-        var lines = File.ReadLines("in.txt");
-        int count = 0;
-
-        foreach (var item in lines)
-        {
-            var s = item.Split('-', ',');
-            var a = new MyRange(Convert.ToInt32(s[0]), Convert.ToInt32(s[1]));
-            var b = new MyRange(Convert.ToInt32(s[2]), Convert.ToInt32(s[3]));
-            var c = new MyRange(Math.Min(a.Start, b.Start), Math.Max(a.End, b.End));
-
-            if (c.Length() <= a.Length() + b.Length())
-            {
-                count++;
-            }
-        }
-
-        Console.WriteLine(count);
-        Console.ReadKey();
+        public int CardId { get; set; }
+        public List<int> WinningNumbers { get; set; } = [];
+        public List<int> DrawnNumbers { get; set; } = [];
     }
 
-    public class MyRange
+
+
+    static void Main()
     {
-        public MyRange(int s, int e)
+        var cards = new List<Card>();
+        var lines = File.ReadLines("in.txt");
+
+        foreach (var line in lines)
         {
-            Start = s; End = e;
+            var c = new Card();
+
+            var split1 = line.Split(':');
+            var split2 = split1[0].Split(' ').Where(f => f.Trim() != "").ToArray();
+
+            c.CardId = Convert.ToInt32(split2[1]);
+
+            var split3 = split1[1].Split('|');
+
+            var split4 = split3[0];
+            var split5 = split3[1];
+
+            var split6 = split4.Trim().Split(' ').Where(f => f.Trim() != "").Select(f => Convert.ToInt32(f));
+            var split7 = split5.Trim().Split(' ').Where(f => f.Trim() != "").Select(f => Convert.ToInt32(f));
+
+            foreach (var item in split6)
+            {
+                c.WinningNumbers.Add(Convert.ToInt32(item));
+            }
+
+            foreach (var item in split7)
+            {
+                c.DrawnNumbers.Add(Convert.ToInt32(item));
+            }
+
+            cards.Add(c);
         }
 
-        public int Start { get; set; }
-        public int End { get; set; }
-
-        public int Length()
+        int worth = 0;
+        foreach (var card in cards)
         {
-            return End - Start;
+            var wins = card.WinningNumbers.Intersect(card.DrawnNumbers).Count();
+            var value = Convert.ToInt32(Math.Pow(2, wins - 1));
+            worth += value;
         }
+
+
+
+        Console.WriteLine(worth);
+        Console.ReadKey();
     }
 }
