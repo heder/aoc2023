07c41e1ba66c5ff3aa34ed275cca7809296988fa
Thomas Heder <thomas.heder@gmail.com>
2023-12-07 07:49:08 +0100

    7

--- 07.1/Program.cs
+++ 07.1/Program.cs
@@ -1,123 +1,101 @@
 ﻿class Program
 {
-    static FsEntry Root = new FsEntry(null, "/") { IsRoot = true, IsDir = true };
-    static FsEntry CurrentDir = Root;
+
+    static Dictionary<char, char> valuemap = new Dictionary<char, char>()
+    {
+        { '2', 'A' },
+        { '3', 'B' },
+        { '4', 'C' },
+        { '5', 'D' },
+        { '6', 'E' },
+        { '7', 'F' },
+        { '8', 'G' },
+        { '9', 'H' },
+        { 'T', 'I' },
+        { 'J', 'J' },
+        { 'Q', 'K' },
+        { 'K', 'L' },
+        { 'A', 'M' }
+    };
+
+
+
+    class Hand
+    {
+        public string Cards { get; set; }
+
+        public string SortableCards { get; set; }
+
+        public int HandValue { get; set; }
+        public int BidValue { get; set; }
+
+        public void EvaluateHand()
+        {
+            var g = Cards.GroupBy(x => x).ToDictionary(g => g.Key, g => g.ToList());
+
+            if (g.Count == 1 && g.Values.Count == 5) // 5
+            {
+                HandValue = 7;
+            }
+            else if (g.Count == 2 && g.Values.Where(f => f.Count == 4).Count() == 1 && g.Values.Where(f => f.Count == 1).Count() == 1)
+            {
+                HandValue = 6;
+            }
+            else if (g.Count == 2 && g.Values.Where(f => f.Count == 3).Count() == 1 && g.Values.Where(f => f.Count == 2).Count() == 1)
+            {
+                HandValue = 5;
+            }
+            else if (g.Count == 3 && g.Values.Where(f => f.Count == 3).Count() == 1 && g.Values.Where(f => f.Count == 1).Count() == 2)
+            {
+                HandValue = 4;
+            }
+            else if (g.Count == 3 && g.Values.Where(f => f.Count == 2).Count() == 2 && g.Values.Where(f => f.Count == 1).Count() == 1)
+            {
+                HandValue = 3;
+            }
+            else if (g.Count == 3 && g.Values.Where(f => f.Count == 2).Count() == 1 && g.Values.Where(f => f.Count == 1).Count() == 3)
+            {
+                HandValue = 2;
+            }
+            else if (Cards.Distinct().Count() == 5)
+            {
+                HandValue = 1;
+            }
+        }
+
+        public void GenerateSortableCards()
+        {
+            for (int i = 0; i < Cards.Length; i++)
+            {
+                SortableCards += valuemap[Cards[i]];
+            }
+        }
+    }
+
+
 
     static void Main()
     {
-        string[] lines = File.ReadAllLines("in.txt").ToArray();
+        string[] lines = File.ReadAllLines("in.txt");
+        List<Hand> hands = [];
 
-        int i = 0;
-        while (i < lines.Length)
+        foreach (string line in lines)
         {
-            var tokens = lines[i].Split(' ');
+            var split = line.Split(' ');
 
-            if (tokens[0] == "$")
-            {
-                switch (tokens[1])
-                {
-                    case "cd":
-                        switch (tokens[2])
-                        {
-                            case "/":
-                                CurrentDir = Root;
-                                break;
-
-                            case "..":
-                                CurrentDir = CurrentDir.Parent;
-                                break;
-
-                            default:
-                                CurrentDir = CurrentDir.FsObjects.Single(f => f.Name == tokens[2]);
-                                break;
-                        }
-                        break;
-
-                    case "ls":
-
-                        i++;
-
-                        while (i < lines.Length && lines[i].StartsWith("$") == false)
-                        {
-                            var fstokens = lines[i].Split(' ');
-
-                            switch (fstokens[0])
-                            {
-                                case "dir":
-                                    CurrentDir.FsObjects.Add(new FsEntry(CurrentDir, fstokens[1]) { IsDir = true });
-                                    break;
-
-                                default:
-                                    CurrentDir.FsObjects.Add(new FsEntry(CurrentDir, fstokens[1]) { IsDir = false, Size = Convert.ToInt32(fstokens[0]) });
-                                    break;
-                            }
-
-
-                            i++;
-                        }
-                        continue;
-
-                    default:
-                        throw new Exception("Unknown command");
-                }
-            }
-
-            i++;
+            var h = new Hand() { Cards = split[0].Trim(), BidValue = Convert.ToInt32(split[1].Trim()) };
+            h.GenerateSortableCards();
+            h.EvaluateHand();
+            hands.Add(h);
         }
 
-        int totalsize = TraverseFilesystem(Root, 1);
+        hands.OrderBy(f => f.HandValue).ThenBy(f => f.SortableCards).ToList();
 
-        Console.WriteLine(d);
-        Console.ReadKey();
-    }
-
-    static int d = 0;
-
-    private static int TraverseFilesystem(FsEntry e, int level)
-    {
-        Console.WriteLine($"{new string('-', level)} {e.Name} (dir)");
-
-        int size = 0;
-        foreach (var item in e.FsObjects)
+        int rank = 1;
+        foreach (var hand in hands)
         {
-            if (item.IsDir)
-            {
-                int dirsize = TraverseFilesystem(item, level + 1);
 
-                if (dirsize <= 100000)
-                {
-                    d += dirsize;
-                }
-
-                size += dirsize;
-            }
-            else
-            {
-                Console.WriteLine($"{new string('-', level + 1)} {item.Name} (file, {item.Size})");
-                size += item.Size;
-            }
         }
 
-        return size;
-    }
 
-    class FsEntry
-    {
-        public FsEntry(FsEntry parent, string name)
-        {
-            Name = name;
-            FsObjects = new List<FsEntry>();
-            Parent = parent;
-        }
-
-        public FsEntry Parent { get; set; }
-
-        public string Name { get; set; }
-
-        public bool IsRoot { get; set; }
-        public bool IsDir { get; set; }
-        public int Size { get; set; }
-
-        public List<FsEntry> FsObjects { get; set; }
-    }
 }
