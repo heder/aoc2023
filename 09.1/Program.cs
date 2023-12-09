c96addf65c7a9e929ca0c0fda8227e3530070a5f
Thomas Heder <thomas.heder@gmail.com>
2023-12-09 11:12:00 +0100

    9

--- 09.1/Program.cs
+++ 09.1/Program.cs
@@ -1,138 +1,61 @@
 ﻿class Program
 {
-    static int yLen = 1000;
-    static int xLen = 1000;
-    static bool[,] visitedByTail;
-    static Pos headPos;
-    static Pos tailPos;
+    class Report
+    {
+        public List<List<int>> Lists { get; set; } = [];
+
+        public void GenerateHistory()
+        {
+            while (true)
+            {
+                var newList = new List<int>();
+                var currentList = Lists.Last();
+
+                for (int j = 0; j < currentList.Count - 1; j++)
+                {
+                    newList.Add(currentList[j + 1] - currentList[j]);
+                }
+
+                Lists.Add(newList);
+
+                if (newList.All(f => f == 0))
+                {
+                    break;
+                }
+            }
+
+            Lists.Last().Add(0);
+
+            for (int i = Lists.Count - 1; i > 0; i--)
+            {
+                int val1 = Lists[i][^1];
+                int val2 = Lists[i - 1].Last();
+
+                Lists[i - 1].Add(val1 + val2);
+            }
+        }
+    }
+
+    static List<Report> reports = [];
 
     static void Main()
     {
-        headPos = new Pos(500, 500);
-        tailPos = new Pos(500, 500);
-        
-        visitedByTail = new bool[xLen, yLen];
-
         string[] lines = File.ReadAllLines("in.txt").ToArray();
 
-        foreach (var line in lines)
+        for (int i = 0; i < lines.Length; i++)
         {
-            var tokens = line.Split(' ');
-            MoveHead(tokens[0], Convert.ToInt32(tokens[1]));
+            var s = lines[i].Split(' ').Where(f => f.Trim() != "").Select(f => Convert.ToInt32(f)).ToList();
 
-            //DumpWorld(500, 510, 500, 510);
+            var r = new Report();
+            r.Lists.Add(s);
+            reports.Add(r);
+
+            r.GenerateHistory();
         }
 
-        yLen = lines.Length;
-        xLen = lines[0].Length;
+        var sum = reports.Sum(f => f.Lists.First().Last());
 
-        var c = visitedByTail.Cast<bool>().Count(f => f == true);
-
-        Console.WriteLine(c);
+        Console.WriteLine(sum);
         Console.ReadKey();
-    }
-
-
-    static void MoveHead(string direction, int steps)
-    {
-        for (int i = 0; i < steps; i++)
-        {
-            MovePos(headPos, direction);
-            MoveTail(tailPos);
-        }
-    }
-
-    static void MoveTail(Pos pos)
-    {
-        // Find "angular" distance to head
-        int d = Math.Abs(headPos.X - tailPos.X) + Math.Abs(headPos.Y - tailPos.Y);
-
-        if (headPos.X != tailPos.X && headPos.Y != tailPos.Y && d == 3)
-        {
-            // Find direction to move tail
-            if (headPos.X > tailPos.X)
-            {
-                MovePos(tailPos, "R");
-            }
-
-            if (headPos.X < tailPos.X)
-            {
-                MovePos(tailPos, "L");
-            }
-
-            if (headPos.Y > tailPos.Y)
-            {
-                MovePos(tailPos, "U");
-            }
-
-            if (headPos.Y < tailPos.Y)
-            {
-                MovePos(tailPos, "D");
-            }
-        }
-        else if ((headPos.X == tailPos.X || headPos.Y == tailPos.Y) && d == 2)
-        {
-            // Find direction to move tail
-            if (headPos.X > tailPos.X)
-            {
-                MovePos(tailPos, "R");
-            }
-
-            if (headPos.X < tailPos.X)
-            {
-                MovePos(tailPos, "L");
-            }
-
-            if (headPos.Y > tailPos.Y)
-            {
-                MovePos(tailPos, "U");
-            }
-
-            if (headPos.Y < tailPos.Y)
-            {
-                MovePos(tailPos, "D");
-            }
-        }
-
-        visitedByTail[tailPos.X, tailPos.Y] = true;
-    }
-
-    private static void MovePos(Pos p, string direction)
-    {
-        switch (direction)
-        {
-            case "U": p.Y++; break;
-            case "L": p.X--; break;
-            case "R": p.X++; break;
-            case "D": p.Y--; break;
-
-            default: break;
-        }
-    }
-
-    private static void DumpWorld(int xmin, int xmax, int ymin, int ymax)
-    {
-        for (int y = ymax; y >= ymin; y--)
-        {
-            for (int x = xmin; x <= xmax; x++)
-            {
-                Console.Write(visitedByTail[x,y] ? "#" : ".");
-            }
-
-            Console.Write(Environment.NewLine);
-        }
-
-        Console.Write(Environment.NewLine);
-    }
-
-    class Pos
-    {
-        public Pos(int x, int y)
-        {
-            X = x; Y = y;
-        }
-
-        public int X { get; set; }
-        public int Y { get; set; }
     }
 }
--- 09.2/Program.cs
+++ 09.2/Program.cs
@@ -1,167 +1,63 @@
-﻿
-class Program
+﻿class Program
 {
-    static int yLen = 1000;
-    static int xLen = 1000;
-    static char[,] world;
-    static bool[,] visitedByTail;
-    static Pos[] ropePositions;
+    class Report
+    {
+        public List<List<int>> Lists { get; set; } = [];
 
+        public void GenerateHistory()
+        {
+            while (true)
+            {
+                var newList = new List<int>();
+                var currentList = Lists.Last();
+
+                for (int j = 0; j < currentList.Count - 1; j++)
+                {
+                    newList.Add(currentList[j + 1] - currentList[j]);
+                }
+
+                Lists.Add(newList);
+
+                if (newList.All(f => f == 0))
+                {
+                    break;
+                }
+
+            }
+
+            Lists.Last().Add(0);
+
+            for (int i = Lists.Count - 1; i > 0; i--)
+            {
+                int val1 = Lists[i][^1];
+                int val2 = Lists[i - 1].Last();
+
+                Lists[i - 1].Add(val1 + val2);
+            }
+        }
+    }
+
+    static List<Report> reports = [];
 
     static void Main()
     {
-        ropePositions = new Pos[10];
-        for (int i = 0; i < 10; i++)
-        {
-            ropePositions[i] = new Pos(500, 500);
-        }
-        
-        visitedByTail = new bool[xLen, yLen];
-
         string[] lines = File.ReadAllLines("in.txt").ToArray();
 
-        foreach (var line in lines)
+        for (int i = 0; i < lines.Length; i++)
         {
-            var tokens = line.Split(' ');
-            Move(tokens[0], Convert.ToInt32(tokens[1]));
+            var s = lines[i].Split(' ').Where(f => f.Trim() != "").Select(f => Convert.ToInt32(f)).ToList();
+            s.Reverse();
+
+            var r = new Report();
+            r.Lists.Add(s);
+            reports.Add(r);
+
+            r.GenerateHistory();
         }
 
-        yLen = lines.Length;
-        xLen = lines[0].Length;
+        var sum = reports.Sum(f => f.Lists.First().Last());
 
-        var c = visitedByTail.Cast<bool>().Count(f => f == true);
-
-        Console.WriteLine(c);
+        Console.WriteLine(sum);
         Console.ReadKey();
-    }
-
-
-    static void Move(string direction, int steps)
-    {
-        for (int i = 0; i < steps; i++)
-        {
-            MovePos(ropePositions[0], direction);
-            //DumpWorld(500, 510, 500, 510);
-
-            for (int r = 1; r < ropePositions.Length; r++)
-            {
-                MoveTail(ropePositions[r], ropePositions[r - 1]);
-                //DumpWorld(500, 510, 500, 510);
-            }
-
-            visitedByTail[ropePositions[9].X, ropePositions[9].Y] = true;
-        }
-    }
-
-    static void MoveTail(Pos pos, Pos relative)
-    {
-        // Find "angular" distance to head
-        int d = Math.Abs(relative.X - pos.X) + Math.Abs(relative.Y - pos.Y);
-
-        if (relative.X != pos.X && relative.Y != pos.Y && d >= 3)
-        {
-            // Find direction to move tail
-            if (relative.X > pos.X)
-            {
-                MovePos(pos, "R");
-            }
-
-            if (relative.X < pos.X)
-            {
-                MovePos(pos, "L");
-            }
-
-            if (relative.Y > pos.Y)
-            {
-                MovePos(pos, "U");
-            }
-
-            if (relative.Y < pos.Y)
-            {
-                MovePos(pos, "D");
-            }
-        }
-        else if ((relative.X == pos.X || relative.Y == pos.Y) && d == 2)
-        {
-            // Find direction to move tail
-            if (relative.X > pos.X)
-            {
-                MovePos(pos, "R");
-            }
-
-            if (relative.X < pos.X)
-            {
-                MovePos(pos, "L");
-            }
-
-            if (relative.Y > pos.Y)
-            {
-                MovePos(pos, "U");
-            }
-
-            if (relative.Y < pos.Y)
-            {
-                MovePos(pos, "D");
-            }
-        }
-    }
-
-
-
-    private static void MovePos(Pos p, string direction)
-    {
-        switch (direction)
-        {
-            case "U": p.Y++; break;
-            case "L": p.X--; break;
-            case "R": p.X++; break;
-            case "D": p.Y--; break;
-
-            default: break;
-        }
-    }
-
-    private static void DumpWorld(int xmin, int xmax, int ymin, int ymax)
-    {
-        world = new char[xLen, yLen];
-
-        for (int y = ymax; y >= ymin; y--)
-        {
-            for (int x = xmin; x <= xmax; x++)
-            {
-                world[x,y] = '.';
-            }
-
-            Console.Write(Environment.NewLine);
-        }
-
-
-        for (int i = 9; i >= 0; i--)
-        {
-            world[ropePositions[i].X, ropePositions[i].Y] = Convert.ToChar(i.ToString());
-        }
-
-        for (int y = ymax; y >= ymin; y--)
-        {
-            for (int x = xmin; x <= xmax; x++)
-            {
-                Console.Write(world[x, y]);
-            }
-
-            Console.Write(Environment.NewLine);
-        }
-
-        Console.Write(Environment.NewLine);
-    }
-
-    class Pos
-    {
-        public Pos(int x, int y)
-        {
-            X = x; Y = y;
-        }
-
-        public int X { get; set; }
-        public int Y { get; set; }
     }
 }
